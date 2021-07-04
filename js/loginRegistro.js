3f5ab922cafa73b0c77ddd1953321877c9f03cef
Nico Cortes <ncortescarbajo@gmail.com>
2021-07-04 11:43:45 +0000

    Se agregó servicio Email JS

--- js/loginRegistro.js
+++ js/loginRegistro.js
@@ -8,76 +8,91 @@ let email = document.querySelector('#mailRegistro');
 let contraseña = document.querySelector('#passwordRegistro');
 
 //Creo la clase para crear las instancias de usuario
-class Usuario{
-    constructor(nombre, apellido, email, contraseña){
-        this.nombre = nombre,
-        this.apellido = apellido,
-        this.email = email,
-        this.contraseña = contraseña,
-        this.verificado = false
-    }
+class Usuario {
+  constructor(nombre, apellido, email, contraseña) {
+    (this.nombre = nombre),
+      (this.apellido = apellido),
+      (this.email = email),
+      (this.contraseña = contraseña),
+      (this.verificado = false);
+  }
 }
 
 //Agregar usuarios
 
-const agregarUsuarios = function(e){
-    e.preventDefault();
-    if(nombre.value && apellido.value && email.value && contraseña.value.length>=8){
-        usuarios.push(
-            new Usuario(
-                nombre.value.toLowerCase(),
-                apellido.value.toLowerCase(),
-                email.value,
-                contraseña.value,
-            )
-        )
-        localStorage.setItem("usuariosKey", JSON.stringify(usuarios));
-        updateDatos();
-        swal({
-            title: "Genial!",
-            text: "Te registraste con éxito",
-            icon: "success",
-          });
-          setTimeout(()=>{
-            location.replace('/login.html')
-        },1500)
-    }else{
-        document.querySelector('#error').className = 'text-center small'
+const agregarUsuarios = function (e) {
+  e.preventDefault();
+  if (nombre.value && apellido.value && email.value && contraseña.value.length >= 8) {
+    usuarios.push(
+      new Usuario(
+        nombre.value.toLowerCase(),
+        apellido.value.toLowerCase(),
+        email.value,
+        contraseña.value
+      )
+    );
+    localStorage.setItem('usuariosKey', JSON.stringify(usuarios));
+
+    // Email JS
+    let templateParams = {
+      user_name: nombre.value.toUpperCase(),
+      destinatario: email.value,
+      email: email.value,
+      password: contraseña.value,
+    };
+
+    emailjs.send('service_yn2yxuy', 'template_zakgkub', templateParams).then(
+      function (response) {
+        console.log('SUCCESS!', response.status, response.text);
+      },
+      function (error) {
+        console.log('FAILED...', error);
+      }
+    );
+
+    updateDatos();
+    swal({
+      title: 'Genial!',
+      text: 'Te registraste con éxito',
+      icon: 'success',
+    });
+    setTimeout(() => {
+      location.replace('/login.html');
+    }, 1500);
+  } else {
+    document.querySelector('#error').className = 'text-center small';
+  }
+};
+
+function updateDatos() {
+  nombre.value = '';
+  apellido.value = '';
+  email.value = '';
+  contraseña.value = '';
+  nombre.className = 'form-control';
+  apellido.className = 'form-control';
+  email.className = 'form-control';
+  contraseña.className = 'form-control';
+}
+
+function validarContraseña(element) {
+  if (element.value.length === 0) {
+    element.className = 'form-control';
+  } else {
+    if (element.value.length < 8) {
+      element.className = 'form-control is-invalid';
+      return false;
+    } else {
+      element.className = 'form-control is-valid';
+      return true;
     }
+  }
 }
 
-function updateDatos(){
-    nombre.value = ""
-    apellido.value = ""
-    email.value = ""
-    contraseña.value = ""
-    nombre.className = 'form-control'
-    apellido.className = 'form-control'
-    email.className = 'form-control'
-    contraseña.className = 'form-control'
+function validarGeneral(element) {
+  if (!element.value) {
+    element.className = 'form-control is-invalid';
+  } else {
+    element.className = 'form-control is-valid';
+  }
 }
-
-function validarContraseña(element){
-if(element.value.length===0){
-element.className='form-control'
-}else{
-    if(element.value.length<8){
-        element.className= 'form-control is-invalid'
-        return false
-    }else{
-        element.className = 'form-control is-valid'
-        return true
-    }
-}
-}
-
-
-function validarGeneral(element){
-if(!element.value){
-element.className='form-control is-invalid'
-}else{
-    element.className = 'form-control is-valid'
-}
-}
-
-
